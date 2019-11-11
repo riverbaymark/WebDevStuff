e6d0562cbb78b7ec67b21a9a7aa7fda9fbca76ef
riverbaymark <mark.pruitt@riverbaysoftworks.com>
2019-11-11 12:11:30 -0500

    Initial commit

---
+++ sjformentry.js
@@ -0,0 +1,13 @@
+<body>
+<form>
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Middle name:<br>
+  <input type="text" name="fmiddlename"><br>
+  Last name:<br>
+  <input type="text" name="lastname"><br>
+  Birth year<br>
+  <input type="text" name="birthyear"><br><br>
+<input type="submit" value="Submit">
+</form>
+</body>
